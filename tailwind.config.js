608eb06564314cffa923b32d4f6ca7950f332617
iman <imancx.cx@gmail.com>
2024-05-07 06:38:01 -0700

    tailwind-tutorial-project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        biggest: "7rem",
+      },
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#888883",
+        },
+        fontFamily: {
+          nunito: ["Nunito"],
+        },
+      },
+    },
+  },
+  plugins: [],
+};
